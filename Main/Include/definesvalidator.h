681c38bce64de281a8c34d8c6152c9067a6c0211
ryfactor <6005108+ryfactor@users.noreply.github.com>
2025-12-15 15:38:23 +1300

    Update ABORT call to use formatted error message

--- Main/Include/definesvalidator.h
+++ Main/Include/definesvalidator.h
@@ -9752,7 +9752,7 @@ class definesvalidator{
 #endif 
 
 
-  if(ssErrors.str().length() > 0) ABORT(ssErrors.str().c_str());
+  if(ssErrors.str().length() > 0) ABORT("Error: %s", ssErrors.CStr());
 
 }};
 
