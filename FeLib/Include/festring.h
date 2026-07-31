3c4eb46f18042cb60b786a32936322a03fdc38e0
ryfactor <6005108+ryfactor@users.noreply.github.com>
2026-07-31 23:07:57 +1200

    Handle reference counting in festring class

--- FeLib/Include/festring.h
+++ FeLib/Include/festring.h
@@ -168,10 +168,18 @@ inline festring::festring(cfestring& Str)
 {
   if(OwnsData)
   {
-    if(REFS(Data) < FESTRING_REF_MAX)
-      ++REFS(Data);
+    if(Data != EmptyString && Size > 0)
+    {
+      if(REFS(Data) < FESTRING_REF_MAX)
+        ++REFS(Data);
+      else
+        CreateOwnData(Str.Data, Str.Size);
+    }
     else
+    {
+      // Ensure we have a proper owned allocation with refcount storage
       CreateOwnData(Str.Data, Str.Size);
+    }
   }
 }
 
@@ -185,8 +193,13 @@ inline festring::festring(sizetype N, char Char)
 inline festring::~festring()
 {
   if(OwnsData)
-    if(!REFS(Data)--)
-      delete [] &REFS(Data);
+  {
+    if(Data != EmptyString && Size > 0)
+    {
+      if(!REFS(Data)--)
+        delete [] &REFS(Data);
+    }
+  }
 }
 
 inline bool festring::operator<(cfestring& Str) const
